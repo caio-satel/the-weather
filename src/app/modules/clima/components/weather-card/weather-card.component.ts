19eef5ae1e297e0212cebaf16e8bb71f04d332bd
Caio Satel <caio_satel@hotmail.com>
2025-02-01 14:52:34 -0300

    feat: criando component card com informações da api

---
+++ src/app/modules/clima/components/weather-card/weather-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { faDroplet, faTemperature0, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from 'src/app/models/interfaces/weatherDatas';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent {
+  // Dados recebidos da previsao do tempo pelo componente pai
+  @Input() weatherDatas!: WeatherDatas;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+
+}
